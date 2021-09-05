850776f49d2f809e444a18a24a507f83c06c37b3
elderschmoellerls@gmail.com <elderschmoellerls@gmail.com>
2021-09-05 14:19:21 -0300

    Inicio HTML, CSS e JavaScript

---
+++ index.js
@@ -0,0 +1,52 @@
+let menu = document.getElementById('cabecalho');
+let corpo = document.getElementById('data-escolha');
+let botao = document.getElementById('botao');
+let data = document.getElementById('data');
+let form = document.getElementById('conteudo');
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// requisicao.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=Hoh0MrwsbihkPDrHJkxApEY2P7MoChNzvetLbW8G');
+// requisicao.send();
+// requisicao.onload = function (){   
+// }
