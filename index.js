84b0f5a2e01967cd37dab4011b17ad5f4312bf99
elderschmoellerls@gmail.com <elderschmoellerls@gmail.com>
2021-09-06 13:45:26 -0300

    Novas modificações HTML, CSS e JavaScript

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-let menu = document.getElementById('cabecalho');
-let corpo = document.getElementById('data-escolha');
+// let corpo = document.getElementById('data-escolha');
+// let data = document.getElementById('data');
+
 let botao = document.getElementById('botao');
-let data = document.getElementById('data');
-let form = document.getElementById('conteudo');
- 
+
+$('#botao').click(function (evento) {
+    evento.prevetDefault();
+    requisicaoApi();
+    console.log('botao clicado');
+})
+
+async function requisicaoApi() {
+    let api = 'es8WIc8UBOONpYHedK2RhfrF8OREDtKw0WqwnKak';
+    let resposta = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api}`);
+    console.log(resposta);
+
+    let data = resposta.json();
+    console.log(data);
+    apiData(data);
+}
+
+function apiData(data) {
+    document.querySelector('#conteudo').innerHTML += data.explanation
+    document.querySelector('#content').innerHTML += `<img src="${data.url}`
+}
 
 
 
@@ -37,16 +56,29 @@ let form = document.getElementById('conteudo');
 
 
 
-
-
-
-
-
-
-
-
-
-// requisicao.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=Hoh0MrwsbihkPDrHJkxApEY2P7MoChNzvetLbW8G');
+// var requisicao = new XMLHttpRequest();
+// requisicao.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=fPFTcM18d1qr2E9XU8Zm89BOtfUDFFOJyNY3w6Xd');
 // requisicao.send();
-// requisicao.onload = function (){   
-// }
+
+// requisicao.onload = function(){{
+//     if (requisicao.status == 200){
+//         var response = requisicao.response;
+//         var responseOBJ = JSON.parse(response);
+//         console.log(responseOBJ);
+//     }
+// }}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
