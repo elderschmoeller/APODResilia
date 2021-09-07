10503338eb09ac752230c919ca0ec1ce90fb0390
elderschmoellerls@gmail.com <elderschmoellerls@gmail.com>
2021-09-07 13:11:59 -0300

    Finalizados HTML e JS

--- index.js
+++ index.js
@@ -1,84 +1,17 @@
-// let corpo = document.getElementById('data-escolha');
-// let data = document.getElementById('data');
-
-let botao = document.getElementById('botao');
-
-$('#botao').click(function (evento) {
-    evento.prevetDefault();
-    requisicaoApi();
-    console.log('botao clicado');
-})
+let botao = document.querySelector('#botao');
 
 async function requisicaoApi() {
-    let api = 'es8WIc8UBOONpYHedK2RhfrF8OREDtKw0WqwnKak';
-    let resposta = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api}`);
+    let key = 'es8WIc8UBOONpYHedK2RhfrF8OREDtKw0WqwnKak';
+    let resposta = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`);
     console.log(resposta);
 
-    let data = resposta.json();
+    let data = await resposta.json();
     console.log(data);
     apiData(data);
 }
 
 function apiData(data) {
     document.querySelector('#conteudo').innerHTML += data.explanation
-    document.querySelector('#content').innerHTML += `<img src="${data.url}`
+    console.log(data.url)
+    document.getElementById('imagem-conteudo').src = data.url;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var requisicao = new XMLHttpRequest();
-// requisicao.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=fPFTcM18d1qr2E9XU8Zm89BOtfUDFFOJyNY3w6Xd');
-// requisicao.send();
-
-// requisicao.onload = function(){{
-//     if (requisicao.status == 200){
-//         var response = requisicao.response;
-//         var responseOBJ = JSON.parse(response);
-//         console.log(responseOBJ);
-//     }
-// }}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
